85fc4f4b2282d7fb5d9f1884250aa39b0d2951c4
KELVIM MUTUKU MUTHAMA <114753155+Taught90@users.noreply.github.com>
2024-05-25 14:08:00 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-let sections = document.querySelectorAll('section');
+let stars = document.querySelector('.stars');
+let intro = document.querySelector('.intro');
+let moon = document.querySelector('.moon');
+let tea = document.querySelector('.tea');
+let comet2 = document.querySelector('.comet-two');
+let comet = document.querySelector('.comet');
 
-window.onscroll = () =>{
-    sections.forEach(sec=> {
-        let top = window.scrollY;
-        let offset = sec.offsetTop -150;
-        let height = sec.offsetHeight;
+let navs = document.querySelectorAll('.nav');
+let csv = document.querySelectorAll('.csv');
+let contact = document.querySelectorAll('.cintact-links');
 
-        if(top >= offset && top < offset + height){
-            sec.classList.add('show-animate');
-        }
-        else{
-            sec.classList.remove('show-animate');
-        }
-    })
-}
+window.addEventListener('scroll', () =>{
+    let value =window.scrollY;
+
+    stars.style.marginTop = value * .5 + 'px';
+    intro.style.marginTop = value * 1.5 + 'px';
+    intro.style.opacity = value * -.002 + 1;
+    intro.style.scale =value * -.0005 + 1;
+    moon.style.scale =value * .002 + 1 ;
+    moon.style.marginTop = value * .6 + 'px';
+    moon.style.rotate = value * .05 + 'deg';
+    comet2.style.marginTop = value * 1.2 + 'px';
+    comet2.style.marginRight = value * 3 + 'px';
+    comet.style.marginTop = value * 1.2 + 'px';
+    comet.style.marginRight = value * 3.5 + 'px';
+
+})
+
+
